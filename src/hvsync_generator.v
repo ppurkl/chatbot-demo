c6f9c0c064137227303ed859372028c6f52a69d2
me9287045-dotcom <me9287045@gmail.com>
2026-07-25 21:36:44 +0300

    Update hvsync_generator.v

--- src/hvsync_generator.v
+++ src/hvsync_generator.v
@@ -2,7 +2,6 @@
 * Copyright (c) 2025 Tiny Tapeout
 * SPDX-License-Identifier: Apache-2.0
 */
-
 module hvsync_generator (
     input  wire clk,
     input  wire reset,
@@ -20,31 +19,26 @@ module hvsync_generator (
     parameter V_TOP     = 33;
     parameter V_BOTTOM  = 10;
     parameter V_SYNC    = 2;
-
     localparam H_SYNC_START = H_DISPLAY + H_FRONT;
     localparam H_SYNC_END   = H_DISPLAY + H_FRONT + H_SYNC - 1;
     localparam H_MAX        = H_DISPLAY + H_BACK + H_FRONT + H_SYNC - 1;
     localparam V_SYNC_START = V_DISPLAY + V_BOTTOM;
     localparam V_SYNC_END   = V_DISPLAY + V_BOTTOM + V_SYNC - 1;
     localparam V_MAX        = V_DISPLAY + V_TOP + V_BOTTOM + V_SYNC - 1;
-
     wire hmaxxed = (hpos == H_MAX) || reset;
     wire vmaxxed = (vpos == V_MAX) || reset;
-
     always @(posedge clk) begin
         if (reset) begin
             hpos  <= 0;
             hsync <= 0;
         end else begin
-            if (hmaxxed) 
+            if (hmaxxed)
                 hpos <= 0;
             else
                 hpos <= hpos + 1;
-
             hsync <= (hpos >= H_SYNC_START && hpos <= H_SYNC_END);
         end
     end
-
     always @(posedge clk) begin
         if (reset) begin
             vpos  <= 0;
@@ -54,10 +48,8 @@ module hvsync_generator (
                 vpos <= 0;
             else
                 vpos <= vpos + 1;
-
             vsync <= (vpos >= V_SYNC_START && vpos <= V_SYNC_END);
         end
     end
-
     assign display_on = (hpos < H_DISPLAY) && (vpos < V_DISPLAY);
 endmodule
